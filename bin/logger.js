dce2e7fab3ded9195e0b77e726bcff229d00587e
david volkov <skvoll.skvoll@gmail.com>
2017-04-14 17:58:00 +0300

    improve console tools

--- bin/logger.js
+++ bin/logger.js
@@ -3,33 +3,47 @@
 require('colors');
 const moment = require('moment');
 
-const fatal = function (message) {
+const fatal = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`FATAL [${moment().format('HH:mm:ss')}]: ${message}`.bgRed);
 
     process.exit();
 };
 
-const error = function (message) {
+const error = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`ERROR [${moment().format('HH:mm:ss')}]: ${message}`.red);
 };
 
-const warn = function (message) {
+const warn = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`WARN [${moment().format('HH:mm:ss')}]: ${message}`.yellow);
 };
 
-const info = function (message) {
+const info = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`INFO [${moment().format('HH:mm:ss')}]: ${message}`.cyan);
 };
 
-const success = function (message) {
+const success = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`SUCCESS [${moment().format('HH:mm:ss')}]: ${message}`.green);
 };
 
-const failed = function (message) {
+const failed = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`FAILED [${moment().format('HH:mm:ss')}]: ${message}`.red);
 };
 
-const log = function (message) {
+const log = function () {
+    let message = Object.values(arguments).join(', ');
+
     console.log(`LOG [${moment().format('HH:mm:ss')}]: ${message}`);
 };
 
