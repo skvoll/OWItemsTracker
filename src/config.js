1426d38f7cb6b950d3dc56c7ac5ab324fb1bfa3e
david volkov <skvoll.skvoll@gmail.com>
2017-04-15 15:29:23 +0300

    v1.0.5

--- src/config.js
+++ src/config.js
@@ -32,7 +32,7 @@ const COLORS = {
 export default class CONFIG {
     static isInitialized = false;
 
-    static VERSION = '1.0.4';
+    static VERSION = '1.0.5';
     static PLATFORM = Platform.OS;
     static PLATFORM_VERSION = Platform.Version;
     static LOCALE = 'en_US';
@@ -49,7 +49,7 @@ export default class CONFIG {
         storageBucket: 'ow-items-tracker-ce744.appspot.com',
     };
 
-    static SHOW_NEWS = true;
+    static SHOW_NEWS = false;
 
     static LANGUAGE = 'en_US';
     static INCLUDE_ICONS_IN_PROGRESS = false;
