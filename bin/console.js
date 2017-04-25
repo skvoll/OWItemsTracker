d3d534ba2b8c6f3b1ae8e874da9f8f349a7b9550
david volkov <skvoll.skvoll@gmail.com>
2017-04-25 13:32:38 +0300

    migration

--- bin/console.js
+++ bin/console.js
@@ -81,3 +81,42 @@ try {
 function temp() {
     logger.fatal(`command not realised`);
 }
+
+function migrate() {
+    const fs = require('fs');
+    const itemsTools = require('./items');
+    const items = require('./../src/data/items.json');
+
+    let output = {}, uid, assoc = {getNew: {}, getOld: {},}, i18n, i18nNew;
+
+    Object.values(items).map((item) => {
+        uid = itemsTools.makeUid(item.type, item.name, item.hero);
+        assoc.getNew[item.uid] = uid;
+        assoc.getOld[uid] = item.uid;
+
+        if (['SPRAY', 'ICON',].indexOf(item.type) !== -1) {
+            fs.renameSync(
+                `./../src/assets/${item.type.toLowerCase()}s/${item.uid}.png`,
+                `./../src/assets/${item.type.toLowerCase()}s/${uid}.png`
+            );
+        }
+
+        item.uid = uid;
+        output[item.uid] = item;
+    });
+
+    fs.readdirSync('./../src/i18n/').map((file) => {
+        if (file.indexOf('.json') !== -1) {
+            i18nNew = {};
+            i18n = require(`./../src/i18n/${file}`);
+            for (let i in i18n.items) if (i18n.items.hasOwnProperty(i)) {
+                i18nNew[assoc.getNew[i]] = i18n.items[i];
+            }
+            i18n.items = i18nNew;
+            fs.writeFileSync(`./../src/i18n/${file}`, JSON.stringify(i18n, null, 2));
+        }
+    });
+
+    fs.writeFileSync('./../src/data/assoc.json', JSON.stringify(assoc, null, 2));
+    fs.writeFileSync('./../src/data/items.json', JSON.stringify(output, null, 2));
+}
